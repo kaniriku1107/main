aec2cf7ddcf347a0dc4bf1bdd5bfd4e2a652b379
kaniriku1107 <kr54811kr@yahoo.co.jp>
2024-03-10 15:34:34 +0900

    [Update]
    共通ヘッダー追加
    アプリケーション起動修正
    新規登録画面、機能追加
    boostrapフレームワーク使用

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.model.Book;
 import com.example.Service.BookService;
+import com.example.form.BookForm;
 
 @Controller
 public class BookController {
@@ -26,5 +29,35 @@ public class BookController {
 		// bookList.htmlの表示
 		return "bookList";
 		
+	}
+	
+	/**
+	 * 新規登録画面を表示
+	 * @param model
+	 * @return 新規登録画面
+	 */
+	@GetMapping("/book-create")
+	public String createBook(Model model) {
+		
+		model.addAttribute("bookForm", new BookForm());
+		
+		return "add";
+		
+	}
+	
+	/**
+	 * データベースに本を登録する
+	 * @param bookForm
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/book-create")
+	public String saveBook(@ModelAttribute BookForm bookForm, Model model) {
+		
+		// 本を登録する
+		service.insert(bookForm);
+		
+		// 本の一覧表示画面にリダイレクト
+		return "redirect:/book-list";
 	}
 }
---
+++ src/main/java/com/example/form/BookForm.java
@@ -0,0 +1,19 @@
+package com.example.form;
+
+import lombok.Data;
+
+@Data
+public class BookForm {
+
+	
+	/**
+	 * 本のタイトルを保持する
+	 */
+	private String title;
+	
+	/**
+	 * 本の値段を保持する
+	 */
+	private Integer price;
+	
+}
